b5724af9b606024850d3b4da8e836e778e069b40
SeanKempt <kempt101@gmail.com>
2022-11-07 22:45:36 -0600

    feat: add preorder postorder inorder functions

--- app.js
+++ app.js
@@ -106,6 +106,58 @@ const findNode = (root, value) => {
   return root;
 };
 
+//performs breadth first search on a given tree
+//putting somefunction here because thats what the assignment called for
+//If some function is not defined then we just return the results array with the values
+const levelOrder = (root, someFunction = null) => {
+  const queue = [];
+  const results = [];
+  if (root === null) {
+    return root;
+  }
+
+  queue.push(root);
+
+  while (queue.length >= 1) {
+    let currentNode = queue[0];
+    if (currentNode.left !== null) {
+      queue.push(currentNode.left);
+    }
+    if (currentNode.right !== null) {
+      queue.push(currentNode.right);
+    }
+    results.push(currentNode);
+    queue.shift(currentNode);
+  }
+
+  if (someFunction === null) {
+    return results;
+  } else {
+    return someFunction;
+  }
+};
+
+//D, L, R
+//leaving as someFunction until I know what function i'm passing to this function
+// need to figure out how to only return the array without having to declare it outside of the function.
+const preOrder = (root, someFunction, results = []) => {
+  if (root === null) return;
+  results.push(root.data);
+  preOrder(root.left, results);
+  preOrder(root.right, results);
+  if (results.length > 0) return results;
+};
+
+//L, D, R
+const inOrder = (root) => {
+  const results = [];
+};
+
+//L, R, D
+const postOrder = (root) => {
+  const results = [];
+};
+
 const testTree = new Tree([2, 5, 10, 20, 5, 3, 100, 44, 100, 6]);
 const testOak = new Tree([30, 50, 70, 60, 80, 40]);
 
@@ -114,3 +166,10 @@ prettyPrint(testTree.root);
 prettyPrint(testOak.root);
 let treeRoot = testTree.root;
 let oakRoot = testOak.root;
+
+const checkTrue = (root) => {
+  if (oakRoot.left === null) {
+    console.log(true);
+  }
+  console.log(oakRoot.left);
+};
