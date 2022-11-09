d794a030849fb7b2d9228351f40f3d23bc5d4f26
SeanKempt <kempt101@gmail.com>
2022-11-09 16:54:58 -0600

    move remaining methods to tree class

--- app.js
+++ app.js
@@ -58,6 +58,155 @@ class Tree {
     }
     return root;
   };
+
+  insertValue = (key, root = this.root) => {
+    if (root === null) return (root = new Node(key));
+    if (key < root.data) {
+      root.left = this.insertValue(key, root.left);
+    } else if (key > root.data) {
+      root.right = this.insertValue(key, root.right);
+    }
+    return root;
+  };
+
+  findNode = (value, root = this.root) => {
+    if (root === null) {
+      return root;
+    } else if (value === root) {
+      return root;
+    } else {
+      if (value < root.data) {
+        root = this.findNode(value, root.left);
+      } else if (value > root.data) {
+        root = this.findNode(value, root.right);
+      }
+    }
+    return root;
+  };
+
+  //performs breadth first search on a given tree
+  //putting somefunction here because thats what the assignment called for
+  //If some function is not defined then we just return the results array with the values
+  levelOrder = (root = this.root, someFunction = null) => {
+    const queue = [];
+    const results = [];
+    if (root === null) {
+      return root;
+    }
+
+    queue.push(root);
+
+    while (queue.length >= 1) {
+      let currentNode = queue[0];
+      if (currentNode.left !== null) {
+        queue.push(currentNode.left);
+      }
+      if (currentNode.right !== null) {
+        queue.push(currentNode.right);
+      }
+      results.push(currentNode);
+      queue.shift(currentNode);
+    }
+
+    if (someFunction === null) {
+      return results;
+    } else {
+      return someFunction;
+    }
+  };
+
+  //D, L, R
+  preOrder = (root = this.root, results = [], callbackFn) => {
+    if (root === null) {
+      return;
+    } else {
+      callbackFn ? callbackFn(root.data) : results.push(root.data);
+      this.preOrder(root.left, results);
+      this.preOrder(root.right, results);
+    }
+    return results;
+  };
+
+  //L, D, R
+  inOrder = (root = this.root, results = [], callbackFn) => {
+    if (root === null) {
+      return;
+    } else {
+      this.inOrder(root.left, results);
+      callbackFn ? callbackFn(root.data) : results.push(root.data);
+      this.inOrder(root.right, results);
+    }
+    return results;
+  };
+
+  //L, R, D
+  postOrder = (root = this.root, results = [], callbackFn) => {
+    if (root === null) {
+      return;
+    } else {
+      this.postOrder(root.left, results);
+      this.postOrder(root.right, results);
+      callbackFn ? callbackFn(root.data) : results.push(root.data);
+    }
+    return results;
+  };
+
+  height = (node) => {
+    if (node === null) {
+      return 0;
+    }
+    let leftHeight = this.height(node.left);
+    let rightHeight = this.height(node.right);
+    return Math.max(leftHeight, rightHeight) + 1;
+  };
+
+  depthRec = (node, root = this.root) => {
+    if (root === null) {
+      return -1;
+    }
+
+    let depth = -1;
+    if (
+      root.data === node ||
+      (depth = this.depthRec(node, root.left)) >= 0 ||
+      (depth = this.depthRec(node, root.right)) >= 0
+    ) {
+      return depth + 1;
+    }
+  };
+
+  isBalanced = (root = this.root) => {
+    let verdict;
+    if (root === null) {
+      return root;
+    } else {
+      let leftHeight = this.height(root.left);
+      let rightHeight = this.height(root.right);
+      if (leftHeight - rightHeight >= 1 || rightHeight - leftHeight >= 1) {
+        verdict = false;
+      } else {
+        verdict = true;
+      }
+    }
+    return verdict;
+  };
+
+  treeToArray = (root = this.root, xArray = []) => {
+    if (root === null) {
+      return root;
+    } else {
+      xArray.push(root.data);
+      if (root.left !== null) this.treeToArray(root.left, xArray);
+      if (root.right !== null) this.treeToArray(root.right, xArray);
+    }
+    return xArray;
+  };
+
+  reBalance = (root = this.root) => {
+    let tree = new Tree(this.treeToArray(root));
+    this.root = tree.root;
+    return tree.root;
+  };
 }
 
 createBST = (array, start = 0, end = array.length - 1) => {
@@ -80,154 +229,6 @@ buildTree = (array) => {
   const rmDuplicates = (array) => [...new Set(array)];
   const filteredArray = rmDuplicates(array).sort(compare);
   return createBST(filteredArray);
-};
-
-const insertValue = (root, key) => {
-  if (root === null) return (root = new Node(key));
-  if (key < root.data) {
-    root.left = insertValue(root.left, key);
-  } else if (key > root.data) {
-    root.right = insertValue(root.right, key);
-  }
-  return root;
-};
-
-const findNode = (root, value) => {
-  if (root === null) {
-    return root;
-  } else if (value === root) {
-    return root;
-  } else {
-    if (value < root.data) {
-      root = findNode(root.left, value);
-    } else if (value > root.data) {
-      root = findNode(root.right, value);
-    }
-  }
-  return root;
-};
-
-//performs breadth first search on a given tree
-//putting somefunction here because thats what the assignment called for
-//If some function is not defined then we just return the results array with the values
-const levelOrder = (root, someFunction = null) => {
-  const queue = [];
-  const results = [];
-  if (root === null) {
-    return root;
-  }
-
-  queue.push(root);
-
-  while (queue.length >= 1) {
-    let currentNode = queue[0];
-    if (currentNode.left !== null) {
-      queue.push(currentNode.left);
-    }
-    if (currentNode.right !== null) {
-      queue.push(currentNode.right);
-    }
-    results.push(currentNode);
-    queue.shift(currentNode);
-  }
-
-  if (someFunction === null) {
-    return results;
-  } else {
-    return someFunction;
-  }
-};
-
-//D, L, R
-const preOrder = (root, results = [], callbackFn) => {
-  if (root === null) {
-    return;
-  } else {
-    callbackFn ? callbackFn(root.data) : results.push(root.data);
-    preOrder(root.left, results);
-    preOrder(root.right, results);
-  }
-  if (results.length > 0) return results;
-};
-
-//L, D, R
-const inOrder = (root, results = [], callbackFn) => {
-  if (root === null) {
-    return;
-  } else {
-    inOrder(root.left, results);
-    callbackFn ? callbackFn(root.data) : results.push(root.data);
-    inOrder(root.right, results);
-  }
-  return results;
-};
-
-//L, R, D
-const postOrder = (root, results = [], callbackFn) => {
-  if (root === null) {
-    return;
-  } else {
-    postOrder(root.left, results);
-    postOrder(root.right, results);
-    callbackFn ? callbackFn(root.data) : results.push(root.data);
-  }
-  return results;
-};
-
-const height = (root) => {
-  if (root === null) {
-    return 0;
-  }
-  let leftHeight = height(root.left);
-  let rightHeight = height(root.right);
-  return Math.max(leftHeight, rightHeight) + 1;
-};
-
-const depthRec = (root, node) => {
-  if (root === null) {
-    return -1;
-  }
-
-  let depth = -1;
-  if (
-    root.data === node ||
-    (depth = depthRec(root.left, node)) >= 0 ||
-    (depth = depthRec(root.right, node)) >= 0
-  ) {
-    return depth + 1;
-  }
-};
-
-const isBalanced = (root) => {
-  let verdict;
-  if (root === null) {
-    return root;
-  } else {
-    let leftHeight = height(root.left);
-    let rightHeight = height(root.right);
-    if (leftHeight - rightHeight >= 1 || rightHeight - leftHeight >= 1) {
-      verdict = false;
-    } else {
-      verdict = true;
-    }
-  }
-  return verdict;
-};
-
-const treeToArray = (root, xArray = []) => {
-  if (root === null) {
-    return root;
-  } else {
-    xArray.push(root.data);
-    if (root.left !== null) treeToArray(root.left, xArray);
-    if (root.right !== null) treeToArray(root.right, xArray);
-  }
-  return xArray;
-};
-
-const reBalance = (root) => {
-  let tree = new Tree(treeToArray(root));
-  return tree.root;
 };
 
 const testTree = new Tree([2, 5, 10, 20, 5, 3, 100, 44, 100, 6]);
